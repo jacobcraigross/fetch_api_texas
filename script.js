45cc7c866f09289a1e8b895ff3929301ad0a0577
Jacob Ross <survivaldawn@gmail.com>
2021-01-21 19:05:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.querySelector("h2").textContent = "Holy Water";
+fetch('texas.jpg').then(response => {
+    return response.blob();
+}).then(blob => {
+    document.getElementById('texxas').src = URL.createObjectURL(blob);
+}).catch(error => {
+    console.log('error!!!!');
+    console.error(error);
+});
